b155732c009121e532a0f691cd1832432092b8b0
Angelo Budil Veronezi <61750998+angeloveronezi@users.noreply.github.com>
2024-05-20 12:30:22 -0300

    Add files via upload

---
+++ LinkedServer.sql
@@ -0,0 +1,20 @@
+USE tsMatriz
+
+SELECT cSituacao, cCodColigada, cCodEmpresaERP, *
+FROM matriz.tsIntegracaoRh.dbo.VW_Colaborador
+WHERE cNome IN ( ''
+               , ''
+               , ''
+			   , ''
+               )
+ORDER BY cNome
+
+
+SELECT cSituacao, cCodColigada, CodEmpresaERP, *
+FROM matriz.tsIntegracaoRh.dbo.VW_Colaborator
+WHERE cNome IN (''
+               , ''
+               , ''
+			   , ''
+               )
+ORDER BY cNome
